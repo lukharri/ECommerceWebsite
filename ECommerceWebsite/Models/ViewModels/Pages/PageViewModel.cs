1d68ecf4e36be45698fc266a61f7b9b9aa3e574f
Luke Harris <luke.e.harris@gmail.com>
2018-01-26 14:46:14 -0600

    Created Pages table, dto, and view model for adding pages from admin area

---
+++ ECommerceWebsite/Models/ViewModels/Pages/PageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ECommerceWebsite.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.Models.ViewModels.Pages
+{
+    public class PageViewModel
+    {
+        public PageViewModel()
+        {
+
+        }
+
+        public PageViewModel(PageDto row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSideBar = row.HasSideBar;
+        }
+        
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+
+        public int Sorting { get; set; }
+
+        public bool HasSideBar { get; set; }
+
+    }
+}
