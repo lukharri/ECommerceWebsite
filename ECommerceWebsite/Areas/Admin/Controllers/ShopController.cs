24711675d05e6e76687a52609988485b1dac3d05
Luke Harris <luke.e.harris@gmail.com>
2018-02-09 13:55:57 -0600

    Added functionality for deleting a merchandise category

--- ECommerceWebsite/Areas/Admin/Controllers/ShopController.cs
+++ ECommerceWebsite/Areas/Admin/Controllers/ShopController.cs
@@ -3,6 +3,7 @@ using ECommerceWebsite.Models.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +11,26 @@ namespace ECommerceWebsite.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop/Categories
+        // GET: Admin/Shop/Index
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<CategoryViewModel> categories;
+
+            using (Db db = new Db())
+            {
+                categories = db.Categories
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new CategoryViewModel(x))
+                    .ToList();
+            }
+
+            return View(categories);
+        }
+
+
+        //GET: Admin/Shop/Categories
         [HttpGet]
         public ActionResult Categories()
         {
@@ -55,5 +75,88 @@ namespace ECommerceWebsite.Areas.Admin.Controllers
             return id;
 
         }
+
+
+        // POST: /Admin/Pages/ReorderCatgegories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // set initial count
+                int count = 1;
+
+                CategoryDto dto;
+
+                // set sorting for each Category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+
+        // GET: /Admin/Pages/DeleteCategory/id
+        [HttpGet]
+        public ActionResult DeleteCategory(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (Db db = new Db())
+            {
+                CategoryDto category = db.Categories.Find(id);
+
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
+
+                CategoryViewModel model = new CategoryViewModel(category);
+
+                return View(model);
+            }
+        }
+
+
+        // POST: /Admin/Shop/DeleteCategory/id
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteCategory(int id)
+        //{
+        //    using (Db db = new Db())
+        //    {
+        //        CategoryDto category = db.Categories.Find(id);
+        //        db.Categories.Remove(category);
+        //        db.SaveChanges();
+        //    }
+
+        //    return RedirectToAction("Categories");
+
+        //}
+
+
+        // GET: /admin/shop/DeleteCategory/id
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                CategoryDto category = db.Categories.Find(id);
+                db.Categories.Remove(category);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Categories");
+
+        }
+
     }
 }
